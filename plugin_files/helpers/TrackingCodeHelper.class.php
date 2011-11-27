d55dcfbe87228ffb69fc02d499c5400929f3ddad
livechat <livechat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-27 18:32:03 +0000

    tagged version 3.2.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-live-chat-software-for-wordpress/tags/3.2.1@468325 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin_files/helpers/TrackingCodeHelper.class.php
+++ plugin_files/helpers/TrackingCodeHelper.class.php
@@ -21,7 +21,7 @@ class TrackingCodeHelper extends LiveChatHelper
 
   (function() {
     var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'labs.livechatinc.com/licence/{$license_number}/script.cgi';
+    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
   })();
 </script>
